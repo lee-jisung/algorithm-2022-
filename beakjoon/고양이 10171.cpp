824e145e08f7863ff37a8bc5a2414439449366e9
leejisung <32594290+lee-jisung@users.noreply.github.com>
2022-05-28 20:13:14 +0900

    Update 고양이 10171.cpp

--- beakjoon/고양이 10171.cpp
+++ beakjoon/고양이 10171.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
-int main(void){
-  
-  
+int main(void)
+{
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << "\\    /\\ \n";
+  cout << " )  ( ')\n";
+  cout << "(  /  )\n";
+  cout << " \\(__)|\n";
+
   return 0;
-  
 }
